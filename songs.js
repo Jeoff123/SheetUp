f59f0a35ee1ed7ade7e997311596350561214492
Jeoff George Giby <164633588+Jeoff123@users.noreply.github.com>
2026-02-28 22:19:28 +0530

    Add files via upload

--- songs.js
+++ songs.js
@@ -106,6 +106,326 @@ const SONGS_DATA = [
         pdfPath: "pdfs/hymns/h13.pdf",
         isFavorite: false
     }
+    ,{
+        id: 14,
+        title: "Rapakal Thorum",
+        subtitle: "14",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h14.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 15,
+        title: "Nalloru Rakshaka Enne",
+        subtitle: "15",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h15.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 16,
+        title: "Karunayulla Yeshuve",
+        subtitle: "16",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h16.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 17,
+        title: "Sandhya Sooryasthamana",
+        subtitle: "17",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h17.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 18,
+        title: "Theernu Pakalkalam",
+        subtitle: "18",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h18.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 19,
+        title: "Ennathmave Vazhthuka Nee",
+        subtitle: "19",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h19.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 20,
+        title: "Elavarum Yeshu Namathe",
+        subtitle: "20",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h20.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 21,
+        title: "Yahova Alathe",
+        subtitle: "21",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h21.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 22,
+        title: "Shristipokheyume",
+        subtitle: "22",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h22.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 23,
+        title: "Yeshu Namam Ethra Embam",
+        subtitle: "23",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h23.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 24,
+        title: "Onnumillaykayil",
+        subtitle: "24",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h24.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 25,
+        title: "Sarvathin Rajanam",
+        subtitle: "25",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h25.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 26,
+        title: "Enne Veenda Rakshakante",
+        subtitle: "26",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h26.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 27,
+        title: "Yeshuve Dyanikumbol",
+        subtitle: "27",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h27.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 28,
+        title: "Raksha Raksha Embaswaram",
+        subtitle: "28",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h28.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 29,
+        title: "Ella Janagalum Vareen",
+        subtitle: "29",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h29.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 30,
+        title: "Papathin Vanvishathe",
+        subtitle: "30",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h30.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 31,
+        title: "Vazhtheen Vazhtheen",
+        subtitle: "31",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h31.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 32,
+        title: "Adtbutharajan Yeshuve",
+        subtitle: "32",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h32.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 33,
+        title: "Attidayer Kumbidum",
+        subtitle: "33",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h33.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 34,
+        title: "Vazhthen Dehi",
+        subtitle: "34",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h34.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 35,
+        title: "Dayvathe Sthudika",
+        subtitle: "35",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h35.pdf",
+        isFavorite: false
+    } 
+    ,{
+        id: 36,
+        title: "Merthyanami Njanyeshuve",
+        subtitle: "36",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h36.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 37,
+        title: "Karthavine Nam Sthutika",
+        subtitle: "37",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h37.pdf",
+        isFavorite: false
+    } 
+    ,{
+        id: 38,
+        title: "Karthavu Varum! Lokavum",
+        subtitle: "38",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h38.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 39,
+        title: "Kandalum Yeshu Varunu",
+        subtitle: "39",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h39.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 40,
+        title: "Kel! Agamanathin Arrpu",
+        subtitle: "40",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h40.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 41,
+        title: "Enne Veendedutha Nathan",
+        subtitle: "41",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h41.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 42,
+        title: "Kartherkahalam",
+        subtitle: "42",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h42.pdf",
+        isFavorite: false
+    }
+    ,{
+        id:43,
+        title: "Dvanikum Andyakahalam",
+        subtitle: "43",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h43.pdf",
+        isFavorite: false
+    }
+    ,{
+        id:44,
+        title: "Va, Varika Emmanuel",
+        subtitle: "44",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h44.pdf",
+        isFavorite: false
+    }
+    ,{
+        id:45,
+        title: "Kalvariyil Enperkaho",
+        subtitle: "45",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h45.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 46,
+        title: "Manavare Reskshichiduvanayi",
+        subtitle: "46",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h46.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 47,
+        title: "Yeshurajan Jenichu",
+        subtitle: "47",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h47.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 48,
+        title: "Yeshu Innu Jenichu",
+        subtitle: "48",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h48.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 49,
+        title: "Bethlehem Nalnagariyil",
+        subtitle: "49",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h49.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 50,
+        title: "Athunathen Kripal",
+        subtitle: "50",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h50.pdf",
+        isFavorite: false
+    }   
+    ,{
+        id:51,
+        title: "Viranjodi Vannidin",
+        subtitle: "51",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h51.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 52,
+        title: "Vishvasikale Vaa",
+        subtitle: "52",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h52.pdf",
+        isFavorite: false
+    }
+    ,{
+        id: 53,
+        title: "Kelkadoothasalswaram",
+        subtitle: "53",
+        category: "hymns",
+        pdfPath: "pdfs/hymns/h53.pdf",
+        isFavorite: false
+    }
 ];
 
 // Function to get songs data
